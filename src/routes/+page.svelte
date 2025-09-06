5d06d694ab6e4acab2e470e46299ce53ec1e72b9
Linh Yen Nguyen <linhyennguyen.work@gmail.com>
2025-09-06 23:59:48 +0700

    chore: init project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+<section class="h-screen flex flex-col justify-center items-center bg-white text-black">
+  <h1 class="text-5xl font-bold mb-4">Xin chào, mình là Linh!</h1>
+  <p class="text-xl mb-6 text-center max-w-xl">
+    Đây là portfolio của mình, làm bằng SvelteKit + TailwindCSS.
+  </p>
+  <a href="#projects" class="px-6 py-3 bg-black text-white rounded hover:bg-gray-800 transition">
+    Xem dự án
+  </a>
+</section>
