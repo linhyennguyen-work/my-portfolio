456cfb204d75ce2bac2fc688dc0cf93df50db313
Linh Yen Nguyen <linhyennguyen.work@gmail.com>
2025-10-21 16:36:36 +0700

    style: init data-theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,10 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
   <p class="text-xl mb-6 text-center max-w-xl">
     Đây là portfolio của mình, làm bằng SvelteKit + TailwindCSS.
   </p>
-  <a href="#projects" class="px-6 py-3 bg-black text-white rounded hover:bg-gray-800 transition">
+  <a href="#projects" class="px-6 py-3 bg-accent text-white rounded hover:bg-gray-800 transition">
     Xem dự án
   </a>
 </section>
